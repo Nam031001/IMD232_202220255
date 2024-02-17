e64d73c2a1889dce615f0c4d5476cf8f3ce13803
Nam031001 <127704144+Nam031001@users.noreply.github.com>
2024-02-18 00:09:51 +0900

    야아아아악

--- src/w5/recording/step15/script/Pendulum.js
+++ src/w5/recording/step15/script/Pendulum.js
@@ -7,26 +7,32 @@ class Pendulum {
   // 진자의 반지름
 
   constructor(x, y, rad, angle, ballRad) {
-    this.angle = angle;
-    this.angleVel = 0;
-    this.angleAcc = 0;
-    this.pos = createVector(x, y);
-    this.rad = rad;
-    this.ballPos = createVector(x, y);
+    this.angle = angle; // 진자의 각도
+    this.angleVel = 0; // 진자의 각도에 대한 속도
+    this.angleAcc = 0; // 진자의 각도에 대한 가속도
+    this.pos = createVector(x, y); // 진자의 중심점
+    this.rad = rad; // 진자의 길이
+    this.ballPos = createVector(x, y); //회색공의 중심
     this.ballPos.add(cos(this.angle) * this.rad, sin(this.angle) * this.rad);
-    this.ballRad = ballRad;
-    this.movingOffset = createVector();
+    //회색공의 위치 : 진자의 길이만큼 떨어져있도록
+
+    this.ballRad = ballRad; // 공의 반지름
+
+    this.movingOffset = createVector(); //클릭해서 이동을 위해선 있어야한다.
     this.isHover = false;
     this.isDragging = false;
   }
 
   applyForce(force) {
     this.angleAcc = (sin(this.angle - (TAU / 360) * 90) * -force.y) / this.rad;
+    // 진자의 각도(힘) = sin(angle) * g / rad 사용
+    // 90도를 기준으로 왔다갔다 하도록 => (TAU / 360) * 90
+    // force가 벡터가 아니기 때문에 y값만 빼서 받아와야 한다.
   }
 
   update() {
     if (!this.isDragging) {
-      this.angleVel *= 0.999;
+      this.angleVel *= 0.999; //점점 속도가 줄어들도록 함
       this.angleVel += this.angleAcc;
       this.angle += this.angleVel;
     }
@@ -67,14 +73,19 @@ class Pendulum {
   }
 
   mouseDragged(mX, mY) {
+    //공의 위치를 직접 우리가 조정하는게 아니라, 해당 각도만 우리가 조정해주는 식으로 해야 오류가 일어나지않는다.
+    //공의 위치는 이미 update에서 설정이 되고 있기 때문
     if (this.isDragging) {
       const ballShouldBe = createVector(
         mX - this.movingOffset.x,
         mY - this.movingOffset.y
+        // 공이 원래 있어야하는 위치 = 마우스 - this.movingOffset
       );
       const angle = atan2(
         ballShouldBe.y - this.pos.y,
         ballShouldBe.x - this.pos.x
+        //공이 향하고자 하는 pos로 방향을 돌린다.
+        //진자의 중심쪽으로 향하는 공
       );
       this.angle = angle;
     }
